465525d85e8998cf2ca1dc059b04b80a971a1cb6
Andres.Namm <andres.namm.001@gmail.com>
2017-12-22 16:34:09 +0200

    something

--- checkCluster.sh
+++ checkCluster.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for cluster_number in {1..4} 
 do 
-ssh -t klaster$cluster_number "journalctl -k | grep error; journalctl -k | grep -i \"stalls on CPUs/tasks\""
+ssh -t klaster$cluster_number "echo "general errors";journalctl -k | grep error; echo "stalls";journalctl -k | grep -i \"stalls on CPUs/tasks\""
 done
