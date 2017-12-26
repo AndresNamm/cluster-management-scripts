a38fdc3262f5882ed7d55cc65bb7a098299a8b6a
andres <andres@machina.regio.ee>
2017-12-26 11:38:01 +0200

    Specified the functions in readme

--- checkCluster.sh
+++ checkCluster.sh
@@ -7,7 +7,7 @@ do
 echo ""
 echo ""
 tput setaf 2
-echo "KLASTER$i"
+echo "KLASTER$cluster_number"
 tput sgr0
 
 ssh -t klaster$cluster_number "echo \"${red}general errors${reset}\";journalctl -k | grep error; echo \"${red}stalls${reset}\";journalctl -k | grep -i \"stalls on CPUs/tasks\""
