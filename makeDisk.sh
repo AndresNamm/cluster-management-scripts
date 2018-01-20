ecb1d063a061c0904d048f4856a49b60196ea94d
andres <andres@klaster3.regio.ee>
2018-01-20 18:31:03 +0200

    add disks

---
+++ makeDisk.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#https://www.digitalocean.com/community/tutorials/how-to-partition-and-format-storage-devices-in-linux
+sudo parted /dev/sdb mklabel gpt && 
+sudo parted -a opt /dev/sdb mkpart primary ext4 0% 100% &&
+lsblk &&
+sudo mkfs.ext4 -L datapartition /dev/sdb1 &&
+sudo mkdir -p /newdisk1 &&
+sudo mount -o defaults /dev/sdb1 /newdisk1 &&
+#sudo echo "LABEL=datapartition /newdisk1 ext4 defaults 0 2" >> /etc/fstab
