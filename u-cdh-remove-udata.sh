4d54dc859891d3bbed181da70d44cf3faeca1fc0
andres <andres@klaster2.regio.ee>
2018-02-01 08:36:10 +0200

    added newdisk to cloudera remove

--- u-cdh-remove-udata.sh
+++ u-cdh-remove-udata.sh
@@ -2,5 +2,5 @@
 #for i in {1..4}
 #do
 rm -Rf /var/lib/flume-ng /var/lib/hadoop* /var/lib/hue /var/lib/navigator /var/lib/oozie /var/lib/solr /var/lib/sqoop* /var/lib/zookeeper
-rm -Rf /dfs /mapred /yarn
+rm -Rf /dfs /mapred /yarn /newdisk1/dfs
 #done
