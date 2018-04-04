e899831872d2bebd24dbb2199e08b23a0ffe7dff
andres <andres@klaster3.regio.ee>
2018-04-04 18:52:22 +0300

    added some delete functionality

---
+++ cloudera/rmInstallDirs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo rm -r /var/log/cloudera-scm-agent
+sudo rm -r /var/lib/cloudera-scm-agent
+sudo rm -r /opt/cloudera
+
+
---
+++ disableASLR.sh
@@ -0,0 +1,1 @@
+echo "kernel.randomize_va_space = 0" > /etc/sysctl.d/01-disable-aslr.conf
