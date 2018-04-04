1126c8c88f677882d36e453a289f38cad863f496
andres <andres@klaster3.regio.ee>
2018-04-04 18:59:26 +0300

    made cloudera uninstaal 1 node based

--- cloudera/u-cdh-remove-lock.sh
+++ cloudera/u-cdh-remove-lock.sh
@@ -1,7 +1,5 @@
-#!/bin/bash
-for i in {1..4}
-do 
-ssh -t root@klaster$i "rm /tmp/.scm_prepare_node.lock"
-done
+#!/bin/bash 
+rm /tmp/.scm_prepare_node.lock
+
 
 
--- cloudera/u-cdh-remove-udata.sh
+++ cloudera/u-cdh-remove-udata.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-for i in {1..4}
-do
 echo "Removing cloudera related data"
-ssh -t root@klaster$i "rm -Rf /var/lib/flume-ng /var/lib/hadoop* /var/lib/hue /var/lib/navigator /var/lib/oozie /var/lib/solr /var/lib/sqoop* /var/lib/zookeeper"
-ssh -t root@klaster$i "rm -Rf /dfs /mapred /yarn /newdisk1/dfs"
-done
+rm -Rf /var/lib/flume-ng /var/lib/hadoop* /var/lib/hue /var/lib/navigator /var/lib/oozie /var/lib/solr /var/lib/sqoop* /var/lib/zookeeper
+rm -Rf /dfs /mapred /yarn /newdisk1/dfs
